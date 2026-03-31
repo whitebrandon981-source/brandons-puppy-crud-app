8e1ea0cbe158e4521e27f8234afa562316ec2b22
whitebrandon981-source <whitebrandon981@gmail.com>
2026-03-31 00:30:50 -0500

    Add Footer component with copyright notice

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="app-footer">
+      <p>&copy; 2026 Brandon White. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
